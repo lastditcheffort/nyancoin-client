d6282da62913e778b91ce4c9a5be3d4b8b21ae34
Shinoa-Fores <btcinfo@sdf.org>
2022-06-02 10:36:56 -0400

    Call va_end() on va_start()ed args - From Bitcoin 5ba61f0.

--- src/zmq/zmqpublishnotifier.cpp
+++ src/zmq/zmqpublishnotifier.cpp
@@ -30,6 +30,7 @@ static int zmq_send_multipart(void *sock, const void* data, size_t size, ...)
         if (rc != 0)
         {
             zmqError("Unable to initialize ZMQ msg");
+            va_end(args);
             return -1;
         }
 
@@ -42,6 +43,7 @@ static int zmq_send_multipart(void *sock, const void* data, size_t size, ...)
         if (rc == -1)
         {
             zmqError("Unable to send ZMQ msg");
+            va_end(args);
             zmq_msg_close(&msg);
             return -1;
         }
@@ -53,6 +55,7 @@ static int zmq_send_multipart(void *sock, const void* data, size_t size, ...)
 
         size = va_arg(args, size_t);
     }
+    va_end(args);
     return 0;
 }
 
