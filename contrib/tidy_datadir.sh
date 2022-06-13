2f48704b7d18d691ba0a0ed0ef54c7e7792411be
Shinoa-Fores <37270226+Shinoa-Fores@users.noreply.github.com>
2022-06-13 10:40:13 -0400

    Update tidy_datadir.sh
    Set bash scripts w/ failsafe.

--- contrib/tidy_datadir.sh
+++ contrib/tidy_datadir.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+#set -eu
+#set -o pipefail
 # Copyright (c) 2013 The Bitcoin Core developers
 # Distributed under the MIT software license, see the accompanying
 # file COPYING or http://www.opensource.org/licenses/mit-license.php.
