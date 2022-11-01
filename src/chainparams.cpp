a06961d347ed962d8d5ec09ac6aa0d8c7de6e93b
Shinoa-Fores <btcinfo@sdf.org>
2022-11-01 17:06:09 -0400

    Missing semicolon in testnet params.

--- src/chainparams.cpp
+++ src/chainparams.cpp
@@ -274,7 +274,7 @@ public:
         consensus.defaultAssumeValid = uint256S("0x6943eaeaba98dc7d09f7e73398daccb4abcabb18b66c8c875e52b07638d93951"); // 900,000
 
         // AuxPoW parameters
-        consensus.nAuxpowChainId = 0xD9 // 217
+        consensus.nAuxpowChainId = 0xD9; // 217
         consensus.fStrictChainId = true;
         consensus.fAllowLegacyBlocks = true;
         consensus.nHeightEffective = 0;
