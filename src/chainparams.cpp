345a127bf946fc1a0e47381e288f098fb1e5d62f
Shinoa-Fores <btcinfo@sdf.org>
2022-12-27 13:48:53 -0500

    Run garbage disposal again

--- src/chainparams.cpp
+++ src/chainparams.cpp
@@ -188,32 +188,6 @@ public:
         fMineBlocksOnDemand = false;
 
         checkpointData = (CCheckpointData) {
-<<<<<<< HEAD
-            boost::assign::map_list_of
-    (         0, uint256S("0xb477d9bc0721a1b96547495404583d68123f471fdd1d4058a9adff2fa7452298"))
-(         30000, uint256S("0xe333edb2a6052a29a0c9f471ec4de5b82a2f8e398fc295db499d2adb1f72b750"))
-(         62000, uint256S("0xc12547453d2a3995893890e0d73cf4b1fe68f1b6b68e0b407547d6050ba0352f"))
-(         81000, uint256S("0x08afecfd7028b3448ce283e236f8a0535da611c6f6942d2062e364fe8ca5f95c"))
-(        100000, uint256S("0x056386351ce37a32a5e6cf3edd90a4a6e4f41a6f1c58d3d4044dfcb762ceb274"))
-(        133000, uint256S("0x2c9ce87324212f2cba4a63840439c173bf16ba1e6ecf9bbf3e8702b828a8d87b"))
-(        222222, uint256S("0x9c732c3be5225afda793fc6e515e4d7c8fc50c76fb376497fb9cd238b320b5d3"))
-(        333333, uint256S("0x2240e1ace3526e5d58e37fd08f12d407bc12887fbebda22b9ff87b39c79abab3"))
-(        505000, uint256S("0xc989c1ec8ef4dabfd10922831826d27a0d2cc95947b2066e385e280b3bc512cb"))
-(       1000000, uint256S("0xdf496e3d3d525d330683d700649116a9b3b97868a35111203c662fff5107f4fe"))
-(	1500000, uint256S("0xbb5610d7628003b3cf480c21f6310468b86dc9ad6e1f659eba795d317b43ffe3"))
-(	2000000, uint256S("0xd8ee546b11171d63f673cdd8f197fd8e474591dda230c4fd6ed64b89410c5058"))
-(	2500000, uint256S("0xdf1983db60f8d1a2ca800b3317d60ce4dc915de56391fb8c5ac0a716eb13763f"))
-(	3000000, uint256S("0xa33706ed9d49a338c171eb9bae11c9dc6fefa41c8a47f5e44cd6e0887eb7e1c6"))
-(	3247562, uint256S("0x83002e09b3ef17b09e9e2d021d7262e7145c7605bfb6ab923640bdcdf24a1206"))		    
-        };
-
-        chainTxData = ChainTxData{
-        
-            1591102676, // * UNIX timestamp of last checkpoint block
-            0,         // * total number of transactions between genesis and last checkpoint
-                      //   (the tx=... number in the SetBestChain debug.log lines)
-            0.5      // * estimated number of transactions per second after checkpoint
-=======
 			boost::assign::map_list_of
 			( 0, uint256S("0xb477d9bc0721a1b96547495404583d68123f471fdd1d4058a9adff2fa7452298"))
 			( 30000, uint256S("0xe333edb2a6052a29a0c9f471ec4de5b82a2f8e398fc295db499d2adb1f72b750"))
